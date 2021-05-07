06e693f342f858ef148abacb1439aff5c4935878
liheng <841878546@qq.com>
2021-05-07 22:16:59 +0800

    study linux

---
+++ test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	printf("hello work study\n");
+	printf("heo work0 study\n");
+	printf("hel work1 study\n");
+	printf("heWOrk2 study\n");
+	printf("hello wORK3 STUDY\n");
+	printf("hello work4 study\n");
+	return 0;
+}
